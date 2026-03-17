556594118b4191c8651f6c8624c5f9eafd019668
NovaStellar-DunEDU <leeamym@dunwoody.edu>
2026-03-17 14:05:23 -0500

    Add files via upload

---
+++ OperationsLibrary/TemperatureConverter.cs
@@ -0,0 +1,21 @@
+﻿namespace OperationsLibrary
+{
+    public class TemperatureConverter
+    {
+
+        private static int baseAdd = 32;
+
+        public static double CtF(double celcius)
+        {
+            double result = (celcius * 1.8) + (baseAdd);
+            return result;
+        }
+
+        public static double FtC(double fahrenheit)
+        {
+            double result = (fahrenheit - baseAdd) * 0.555;
+            return result;
+        }
+
+    }
+}
